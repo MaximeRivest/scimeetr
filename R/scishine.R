779be34a31161114350f861f95828954a9b70b3e
Maxime Rivest <mrive052@gmail.com>
2018-08-26 21:20:18 -0400

    add rl and sub-coms

--- R/scishine.R
+++ R/scishine.R
@@ -19,10 +19,15 @@ scishine <- function(){
       sidebarMenu(
         menuItem("Summary", tabName = "summary", icon = icon("home")),
         menuItem("In depth characterization", tabName = "characterize", icon = icon("star")),
+        menuItem("Reading list", tabName = "reading_list", icon = icon("book")),
         shiny::fileInput(
           inputId = 'file_directory',
           label = NULL,multiple = T,buttonLabel = 'Browse',
-          placeholder = "Text file")
+          placeholder = "Text file"),
+        shiny::radioButtons(
+          inputId = 'coms',
+          label = 'Select Community',
+          choices = list('com1' = 1))
       )),
     dashboardBody(
       tabItems(
@@ -30,7 +35,7 @@ scishine <- function(){
                 # Boxes need to be put in a row (or column)
                 fluidRow(
                   box(visNetwork::visNetworkOutput("subcomplot"), 
-                                      width = 4),
+                      width = 4),
                   box(DT::dataTableOutput("comlegend"), width = 8)
                 ),
                 fluidRow(
@@ -40,7 +45,7 @@ scishine <- function(){
                   valueBoxOutput("mean_nb_citation"),
                   valueBoxOutput("med_nb_citation"),
                   valueBoxOutput("nb_subcom")
-                  ),
+                ),
                 fluidRow(
                   box(networkD3::sankeyNetworkOutput("plot_sum_com_tag"),
                       width = 12)
@@ -81,20 +86,53 @@ scishine <- function(){
                                width = 12))
                   )
                 )
+        ),
+        tabItem("reading_list",
+                fluidRow(
+                  tabBox(
+                    title = "Reading Lists",
+                    id = "tabset3", height = "700px", width = "400px",
+                    tabPanel("Core papers", 
+                             box(
+                               DT::dataTableOutput("rl_core_papers"), 
+                               width = 12)),
+                    tabPanel("Core year", 
+                             box(
+                               sliderInput(inputId = "nb_paper_yr",
+                                           label = "Number of papers per year:",
+                                           min = 1,
+                                           max = 5,
+                                           value = 1)
+                             ),
+                             box(
+                               DT::dataTableOutput("rl_core_yr"), 
+                               width = 12)),
+                    tabPanel("Old classics", 
+                             box(
+                               DT::dataTableOutput("rl_classics"), 
+                               width = 12)),
+                    tabPanel("Reviews", 
+                             box(
+                               DT::dataTableOutput("rl_reviews"), 
+                               width = 12))
+                  )
+                )
         )
-      )
-    )
+      ), skin = "purple"
+    ),
+    skin = "purple"
   )
   
   # Define server logic required to draw a histogram ----
-  server <- function(input, output) {
+  server <- function(input, output, session) {
     lsci <- reactive({
       if(!is.null(input$file_directory)){
-        sci <- import_wos_files_shine(input$file_directory$datapath)
+        sci <- scimeetr:::import_wos_files_shine(input$file_directory$datapath)
       } else {
         sci <- NULL
       }
     })
+    
     lsci1 <-  reactive({
       if(is.null(lsci())){
         lsci1 <- NULL
@@ -102,6 +140,7 @@ scishine <- function(){
         scimap(lsci())
       }
     })
+    
     r <-  reactive({
       if(is.null(lsci1())){
         r <- NULL
@@ -110,45 +149,71 @@ scishine <- function(){
       }
     })
     
+    observe({
+      choicelist <- as.list(1:length(names(lsci1())))
+      names(choicelist) <- paste0( '* ',
+                                   purrr::map_chr(
+                                     purrr::map(purrr::map(
+                                       lsci1(),
+                                       'tag'), function(x){x[1:3]}),
+                                     paste,
+                                     collapse = ' * ',
+                                     sep = ' * '),
+                                   ' (',
+                                   toupper(names(lsci1())), ')')
+      # Can also set the label and select items
+      updateRadioButtons(session, "coms",
+                         choices = choicelist,
+                         selected = 1
+      )
+    })
+    
+    output$tst <- renderValueBox({
+      valueBox(
+        input$coms, "papers", icon = icon("article"),
+        color = "purple", width = 1
+      )
+    })
+    
     output$nb_papers <- renderValueBox({
       valueBox(
         r()$nb_papers, "papers", icon = icon("article"),
-        color = "light-blue", width = 1
+        color = "purple", width = 1
       )
     })
     
     output$nb_ref <- renderValueBox({
       valueBox(
         r()$nb_ref, "unique references", icon = icon("article"),
-        color = "light-blue", width = 1
+        color = "purple", width = 1
       )
     })
     
     output$avg_nb_ref <- renderValueBox({
       valueBox(
         r()$avg_nb_ref, "reference per paper on average", icon = icon("article"),
-        color = "light-blue", width = 1
+        color = "purple", width = 1
       )
     })
     
     output$mean_nb_citation <- renderValueBox({
       valueBox(
         round(r()$mean_nb_citation[1]), "citation per paper on average", icon = icon("article"),
-        color = "light-blue", width = 1
+        color = "purple", width = 1
       )
     })
     
     output$med_nb_citation <- renderValueBox({
       valueBox(
         median(r()$quant_nb_citation[3]), "citation per paper on median", icon = icon("article"),
-        color = "light-blue", width = 1
+        color = "purple", width = 1
       )
     })
     
     output$nb_subcom <- renderValueBox({
       valueBox(
         length(lsci1())-1, "communities", icon = icon("article"),
-        color = "light-blue", width = 1
+        color = "purple", width = 1
       )
     })
     
@@ -223,18 +288,19 @@ scishine <- function(){
         data.frame("Keywords" = NA,
                    "Frequency" = NA)
       } else {
-        scimeetr::characterize_kw(lsci())$com1
+        scimeetr::characterize_kw(lsci1())[[as.numeric(input$coms)]]
       },
       options = list(
         pageLength = 6
       ))
     
     output$au <- DT::renderDataTable(
+      
       if(is.null(lsci())){
         data.frame("Keywords" = NA,
                    "Frequency" = NA)
       } else {
-        scimeetr::characterize_au(lsci())$com1
+        au <- scimeetr::characterize_au(lsci1())[[as.numeric(input$coms)]]
       },
       options = list(
         pageLength = 6
@@ -245,7 +311,7 @@ scishine <- function(){
         data.frame("Journals" = NA,
                    "Frequency" = NA)
       } else {
-        scimeetr::characterize_jo(lsci())$com1
+        scimeetr::characterize_jo(lsci1())[[as.numeric(input$coms)]]
       },
       options = list(
         pageLength = 6
@@ -256,7 +322,7 @@ scishine <- function(){
         data.frame("Title words" = NA,
                    "Frequency" = NA)
       } else {
-        scimeetr::characterize_ti(lsci())$com1
+        scimeetr::characterize_ti(lsci1())[[as.numeric(input$coms)]]
       },
       options = list(
         pageLength = 6
@@ -267,7 +333,7 @@ scishine <- function(){
         data.frame("Abstractwords" = NA,
                    "Frequency" = NA)
       } else {
-        scimeetr::characterize_ab(lsci())$com1
+        scimeetr::characterize_ab(lsci1())[[as.numeric(input$coms)]]
       },
       options = list(
         pageLength = 6
@@ -278,7 +344,7 @@ scishine <- function(){
         data.frame("Countries" = NA,
                    "Frequency" = NA)
       } else {
-        scimeetr::characterize_co(lsci())$com1
+        scimeetr::characterize_co(lsci1())[[as.numeric(input$coms)]]
       },
       options = list(
         pageLength = 6
@@ -289,10 +355,62 @@ scishine <- function(){
         data.frame("Universities" = NA,
                    "Frequency" = NA)
       } else {
-        scimeetr::characterize_un(lsci())$com1
+        scimeetr::characterize_un(lsci1())[[as.numeric(input$coms)]]
       },
       options = list(
         pageLength = 6
+      ))
+    
+    output$rl_core_papers <- DT::renderDataTable(
+      if(is.null(lsci())){
+        data.frame("Paper" = NA,
+                   "Citation number" = NA)
+      } else {
+        scimeetr::scilist(lsci1(), 
+                          reading_list = 'core_papers',
+                          k = 100)[[as.numeric(input$coms)]]
+      },
+      options = list(
+        pageLength = 20
+      ))
+    
+    output$rl_core_yr <- DT::renderDataTable(
+      if(is.null(lsci())){
+        data.frame("Paper" = NA,
+                   "Citation number" = NA)
+      } else {
+        scimeetr::scilist(lsci1(), 
+                          reading_list = 'core_yr',
+                          k = input$nb_paper_yr)[[as.numeric(input$coms)]]
+      },
+      options = list(
+        pageLength = 20
+      ))
+    
+    output$rl_classics <- DT::renderDataTable(
+      if(is.null(lsci())){
+        data.frame("Paper" = NA,
+                   "Citation number" = NA)
+      } else {
+        scimeetr::scilist(lsci1(), 
+                          reading_list = 'core_residual',
+                          k = 20)[[as.numeric(input$coms)]]
+      },
+      options = list(
+        pageLength = 20
+      ))
+    
+    output$rl_reviews <- DT::renderDataTable(
+      if(is.null(lsci())){
+        data.frame("Paper" = NA,
+                   "Citation number" = NA)
+      } else {
+        scimeetr::scilist(lsci1(), 
+                          reading_list = 'cite_most_others',
+                          k = 20)[[as.numeric(input$coms)]]
+      },
+      options = list(
+        pageLength = 20
       ))
   }
   
